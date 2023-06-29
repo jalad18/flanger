09014ee80c0288ff9cedc2c27ec44e0b0badc312
Ishika <ishika.upadhyay@preciousinfosystem.com>
2023-06-29 15:00:37 +0530

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :line_items
+  resources :carts
+  resources :instruments
+  devise_for :users, controllers: {
+    registrations: 'registrations'
+  }
+  root 'instruments#index'
+  
+end
---
+++ db/migrate/20230614054454_add_user_to_instruments.rb
@@ -0,0 +1,5 @@
+class AddUserToInstruments < ActiveRecord::Migration[7.0]
+  def change
+    add_column :instruments, :user_id, :integer
+  end
+end
